df35a22ea5c8c32c50e935e89d00bba6aa193c33
Adam Gibbons <adam.d.gibbons@gmail.com>
2018-10-01 16:07:31 -0600

    begin area chart

---
+++ src/components/AreaChart.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="chart" ref="chartdiv">
+  </div>
+</template>
+
+<script>
+
+import * as am4core from "@amcharts/amcharts4/core"
+import * as am4charts from "@amcharts/amcharts4/charts"
+import timeSeries from "@/data/timeSeries.js"
+
+export default {
+  name: 'AreaChart',
+  mounted() {
+    // Create chart instance
+    let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+
+    // Add data
+    chart.data = timeSeries
+
+    // Create axes
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis())
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+    // Create series
+    let series = chart.series.push(new am4charts.LineSeries());
+    series.name = "Units";
+    series.stroke = am4core.color("darkorange");
+    series.strokeWidth = 3;
+    series.dataFields.valueY = "prevalence";
+    series.dataFields.dateX = "date";
+  },
+  beforeDestroy() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.chart {
+  width: 100%;
+  height: 500px;
+}
+</style>
---
+++ src/data/timeSeries.js
@@ -0,0 +1,28 @@
+export default [{
+  "prevalence": 1,
+  "date": new Date(2015, 1)
+}, {
+  "prevalence": 25,
+  "date": new Date(2018, 2, 20)
+}, {
+  "prevalence": 33,
+  "date": new Date(2018, 3, 20)
+}, {
+  "prevalence": 4,
+  "date": new Date(2018, 4, 20)
+}, {
+  "prevalence": 5,
+  "date": new Date(2018, 5, 20)
+}, {
+  "prevalence": 75,
+  "date": new Date(2018, 6, 20)
+}, {
+  "prevalence": 89,
+  "date": new Date(2018, 7, 20)
+}, {
+  "prevalence": 95,
+  "date": new Date(2018, 8, 20)
+}, {
+  "prevalence": 99,
+  "date": new Date(2018, 11, 20)
+}]
